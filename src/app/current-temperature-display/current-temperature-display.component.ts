2b62c5e5b1bc55bc7b74197af0e131150e19f346
Korhan <korhan.kilinckaya@insightsoftware.com>
2023-10-16 02:00:50 +0300

    latest commits

--- src/app/current-temperature-display/current-temperature-display.component.ts
+++ src/app/current-temperature-display/current-temperature-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'current-temperature-display',
@@ -6,13 +7,24 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./current-temperature-display.component.css']
 })
 export class CurrentTemperatureDisplayComponent implements OnInit {
-  @Input() minimum: number = 0;
-  @Input() maximum: number = 0;
-  @Input() target: number = 0;
+  @Input() minimum!: number;
+  @Input() maximum!: number;
+  @Input() target!: number;
+  leftLegRotation: number = 30;
+  rightLegRotation: number = 330;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  calculatedRadiusRotation = (): number => {
+    let displayArea = this.rightLegRotation - this.leftLegRotation;
+    let scaledDisplayAreaForInputs = this.maximum - this.minimum;
+
+    let radiusCoefficient = (this.target - this.minimum)/scaledDisplayAreaForInputs;
+
+    let radiusPlacement = (displayArea*radiusCoefficient) + this.leftLegRotation;
+
+    return radiusPlacement;
+  };
 }
